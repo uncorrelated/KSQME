db89b247ad8e7fc84112a8134c2daa5a60057e1a
uncorrelated <uncorrelated@yahoo.co.jp>
2020-09-12 00:32:58 +0900

    コードを整理する

--- CH8/which.c
+++ CH8/which.c
@@ -3,7 +3,7 @@
 #include "which.h"
 
 /*
-	a ∈ [array[0], array[size-1]]でarray[m] ≤ a ≤ array[m+1]となるmを返す
+	a ∈ [array[0], array[size-1]]でarray[m] ≤ a < array[m+1]となるmを返す
 	a < array[0]のときは0、a > array[size-1]のときはsize-1
 */
 int	which(double *array, int size, double a)
@@ -12,14 +12,16 @@ int	which(double *array, int size, double a)
 	
 	l = 0;
 	r = size - 1;
+	/*
+		if(a >= array[size-1]) return size-1; をループ前につけて、
+		while(l + 1 < r){ にして、if(a < array[m + 1]){ ... }を無くして、
+		l = m + 1;をl = m;にした方がたぶん速い。
+	*/
 	while(l < r){
 		m = (l + r)/2;
-		if(a == array[m]){
-			return m;
-		}
 
-		if(a > array[m]){
-			if(a < array[(m + 1) % size]){
+		if(a >= array[m]){
+			if(a < array[m + 1]){ /* 端数切り捨てによりmは必ずm<size-1 */
 				return m;
 			}
 			l = m + 1;/* int型変数を/2をすると切り捨てになるので、下端を動かすときは+1しないと収束しない */
