2ff98db12f02d47037991554f411151c2683582a
uncorrelated <uncorrelated@yahoo.co.jp>
2020-10-07 18:46:30 +0900

    不要なincludeを消す

--- CH8/MNS2016/src/pchip.h
+++ CH8/MNS2016/src/pchip.h
@@ -2,7 +2,6 @@
 #define _HermiteInterpolation 1
 
 #include <Rcpp.h>
-#include <RcppParallel.h>
 
 double pchip(Rcpp::NumericVector knot_x, Rcpp::NumericVector knot_y, double x);
 double pchip(const double *knot_x, const double *knot_y, const int kn, const double x);
